c5c75b6d4c34b22bd440bc7f7caa0daeb9d1d170
rchrdho <richardho16@hotmail.com>
2025-12-01 12:22:55 -0800

    added caching. revised the user account UI and displaying. Added Display Name property for the user to display so they won't get spam emailed. Implemented profile pictures, currently its our default, I have yet to finish the upload its just file explorer opening rn. Im sure theres more I did and am forgetting

--- GameCatcher/Data/Migrations/20251201150652_AddDisplayNameColumn.Designer.cs
+++ GameCatcher/Data/Migrations/20251201150652_AddDisplayNameColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GameCatcher.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GameCatcher.Data.Migrations
 {
     [DbContext(typeof(GameCatcherDbContext))]
-    partial class GameCatcherDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251201150652_AddDisplayNameColumn")]
+    partial class AddDisplayNameColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
---
+++ GameCatcher/Data/Migrations/20251201150652_AddDisplayNameColumn.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameCatcher.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDisplayNameColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "GameId",
+                table: "Reviews",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "GameId",
+                table: "Reviews",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "INTEGER");
+        }
+    }
+}
