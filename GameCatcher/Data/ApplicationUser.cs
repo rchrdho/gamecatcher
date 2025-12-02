11b2817fdb57a9d5891b388525e3524237e227cf
HoRichard <richardho16@hotmail.com>
2025-12-01 22:38:20 -0800

    Completed User Profile aswell as dynamically loaded in profile picture to the nav bar but with a Caveat. With how we handle it using claims and cookies theres a delay when u update your profile picture, it won't update immediately on the navbar. I am so tired I dont remember what else I did tbh. But yeah woo!

--- GameCatcher/Data/ApplicationUser.cs
+++ GameCatcher/Data/ApplicationUser.cs
@@ -9,31 +9,36 @@ namespace GameCatcher.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
-    public required string? DisplayName {get; set;}
+    public required string? DisplayName { get; set; }
     public string? ProfilePictureUrl { get; set; }
     public List<Review>? Reviews { get; set; }
     public virtual ICollection<ApplicationUser>? Friends { get; set; }
     public virtual ICollection<ApplicationUser>? FriendOf { get; set; }
-
-
 }
+
 public class CustomUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<ApplicationUser>
 {
     public CustomUserClaimsPrincipalFactory(
         UserManager<ApplicationUser> userManager,
-        IOptions<IdentityOptions> optionsAccessor)
-        : base(userManager, optionsAccessor)
-    {}
-        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(ApplicationUser user)
-        {
-            var identity = await base.GenerateClaimsAsync(user);
-            identity.AddClaim(new Claim("FactoryTest", "It Works!"));
-            
-            if (!string.IsNullOrEmpty(user.DisplayName))
-            {
-                identity.AddClaim(new Claim("DisplayName", user.DisplayName));
-            }
+        IOptions<IdentityOptions> optionsAccessor
+    )
+        : base(userManager, optionsAccessor) { }
 
-            return identity;
+    protected override async Task<ClaimsIdentity> GenerateClaimsAsync(ApplicationUser user)
+    {
+        var identity = await base.GenerateClaimsAsync(user);
+        identity.AddClaim(new Claim("FactoryTest", "It Works!"));
+
+        if (!string.IsNullOrEmpty(user.DisplayName))
+        {
+            identity.AddClaim(new Claim("DisplayName", user.DisplayName));
         }
+
+        if (!string.IsNullOrEmpty(user.ProfilePictureUrl))
+        {
+            identity.AddClaim(new Claim("ProfilePictureUrl", user.ProfilePictureUrl));
+        }
+
+        return identity;
     }
+}
